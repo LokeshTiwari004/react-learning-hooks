eac43354631811e6b0e8b3d040941eb6184b7b9d
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2022-06-29 14:10:41 +0530

    ..

---
+++ src/Components/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <button onClick={setCount(count + 1)}> Count {count}</button>
+  )
+}
+
+export default Counter
